5211d25e28f3d4838df38a8e11b2685311eeb8d8
Maksim Vladykin <m.a.vladykin@gmail.com>
2017-10-17 19:10:36 +0500

    Update multisig wallet

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const ICO = artifacts.require("./ICO.sol");
-const MSig = artifacts.require("./installed/wallet.sol");
+const MSig = artifacts.require("./installed/MultiSigWallet.sol");
 
 module.exports = (deployer, network) => {
   if (network === "development") {
@@ -15,7 +15,7 @@ module.exports = (deployer, network) => {
       .then(msig => deployer.deploy(ICO, msig.address));
   }
   else if (network === "office") {
-    const team =  [ "0x3D4058006004d4A00cd562BC34EaD0e61AcF9593" , "0xa73a6822EbF4b3d1930871408b1456186083bC46" , "0x3f799D9f7104B4Ca31A048aba98cd494Ecd4ED09" ];
+    const team =  [ "0xD0efEC4aF19bc3D468D4E9271AC462C4169440c2" , "0x0db11bc3c1538f274ad23b626253d951394834b1" , "0x5b4df46f7b715c208ad2b26be93aa30fbdfab80b" ];
     const requiredConfirmations = 2;
 
     deployer.deploy(MSig, team, requiredConfirmations)
